365964b255cf35bcc051ebdeea83c377e78edfb2
hoangloc1307@gmail.com <hoang.loc@pcv.com.vn>
2023-02-28 09:45:10 +0700

    Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import { getSortedPostsData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export async function getStaticProps() {
     const allPostsData = getSortedPostsData();
@@ -13,7 +15,6 @@ export async function getStaticProps() {
 }
 
 export default function Home({ allPostsData }) {
-    console.log(allPostsData);
     return (
         <Layout home>
             <Head>
@@ -37,11 +38,11 @@ export default function Home({ allPostsData }) {
                 <ul className={utilStyles.list}>
                     {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
-                            {title}
+                            <Link href={`/posts/${id}`}>{title}</Link>
                             <br />
-                            {id}
-                            <br />
-                            {date}
+                            <small className={utilStyles.lightText}>
+                                <Date dateString={date} />
+                            </small>
                         </li>
                     ))}
                 </ul>
